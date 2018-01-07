ade409ddca57ea4a87bb77459d2634d61497172a
Fabio Lopes <fzlopes1@gmail.com>
2018-01-07 15:31:29 -0200

    Salvar o arquivo AddressCreateRequest.php.

--- app/Http/Requests/AddressCreateRequest.php
+++ app/Http/Requests/AddressCreateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AdressCreateRequest extends FormRequest
+class AddressCreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
